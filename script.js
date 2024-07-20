869726800dc3fee3c553826e095609b386f3b56e
SYLVAIN <akpalousylvain380@gmail.com>
2024-07-20 14:48:55 +0000

    Tradin-Bot

---
+++ script.js
@@ -0,0 +1,5 @@
+function buyNow() {
+    alert("Merci pour votre confiance ! Vous allez être redirigé vers la page de paiement.");
+    // Redirection vers une page de paiement ou autre action
+    window.location.href = "t.me/Sylvain_akp.html"; // Exemple de redirection
+}
